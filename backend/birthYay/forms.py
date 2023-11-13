c3e9a147a66fbd54414fffe14be8b67a916f3482
sophiexie <sophiexie118@gmail.com>
2023-11-12 22:20:17 -0500

    Added CustomUserCreationForm.

--- backend/birthYay/forms.py
+++ backend/birthYay/forms.py
@@ -1,11 +1,19 @@
-from django.contrib import admin
-from .models import User, Gift, Link, Wishlist, WishlistItem
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
-class GiftAdminForm(forms.ModelForm):
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, required=True, help_text='Required')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required')
+    birthday = forms.DateField(input_formats=('%Y/%m/%d', '%d/%m/%Y', '%Y-%m-%d', '%d-%m-%Y'), required=True, help_text='Required')
+    profile_photo = forms.ImageField(allow_empty_file=True, required=False)
+
     class Meta:
-        model = Gift
-        fields = '__all__'
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'birthday', 'profile_photo')
+
 
     def __init__(self, *args, **kwargs):
         super(GiftAdminForm, self).__init__(*args, **kwargs)
