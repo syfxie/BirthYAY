56f567963aebe246efc9dbff750891204353d411
sophiexie <sophiexie118@gmail.com>
2023-11-12 22:26:17 -0500

    Added URLs for all API views.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,15 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-    
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from birthYay import views
+
+router = routers.DefaultRouter()
+router.register(r"gifts/(?P<gift_id>[^/.]+)/links", views.LinkViewSet, basename='gift-link')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('birthYay.urls')),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
---
+++ backend/birthYay/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('users/', views.CustomUserList.as_view(), name='user-list'),
+    path('users/<uuid:pk>/', views.CustomUserDetails.as_view(), name='user-detail'),
+    path('users/<uuid:pk>/change_password/', views.ChangePasswordView.as_view(), name='change-password'),
+    path('users/<uuid:pk>/follow/', views.FollowUserView.as_view(), name='follow-user'),
+    path('gifts/', views.GiftList.as_view(), name='gift-list'),
+    path('gifts/<uuid:pk>/', views.GiftDetails.as_view(), name='gift-detail'),
+]
