fad57ac449fe047d596507665c8aa3dc34243279
sophiexie <sophiexie118@gmail.com>
2023-11-19 23:19:23 -0500

    Add urls.

--- backend/birthYay/urls.py
+++ backend/birthYay/urls.py
@@ -5,8 +5,11 @@ from . import views
 urlpatterns = [
     path('users/', views.CustomUserList.as_view(), name='user-list'),
     path('users/<uuid:pk>/', views.CustomUserDetails.as_view(), name='user-detail'),
-    path('users/<uuid:pk>/change_password/', views.ChangePasswordView.as_view(), name='change-password'),
+    path('users/<uuid:pk>/change-password/', views.ChangePasswordView.as_view(), name='change-password'),
     path('users/<uuid:pk>/follow/', views.FollowUserView.as_view(), name='follow-user'),
     path('gifts/', views.GiftList.as_view(), name='gift-list'),
     path('gifts/<uuid:pk>/', views.GiftDetails.as_view(), name='gift-detail'),
+    path('get-token/', views.CustomAuthToken.as_view()),
+    path('get-current-user/', views.get_current_user, name='get_current_user'),
+    path('upcoming-birthdays/', views.get_upcoming_birthdays, name='get_upcoming_birthdays')
 ]
