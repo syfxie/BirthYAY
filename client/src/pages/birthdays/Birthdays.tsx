7a92c7280d12207774748a88565aeb5b8fa95f0b
sophiexie <sophiexie118@gmail.com>
2024-01-18 20:36:43 -0500

    Added birthday and Gift pages with template users/gifts.

--- client/src/pages/birthdays/Birthdays.tsx
+++ client/src/pages/birthdays/Birthdays.tsx
@@ -1,7 +1,46 @@
-export default function Birthdays() {
-    return (
-        <div>
-            <h1>Coming Soon</h1>
+import * as React from "react";
+
+import Button from '../../components/UI/Button'
+import SideBar from "../../components/SideBar";
+import BirthdayCard from "../../components/BirthdayCard";
+
+export default function Birthday() {
+    console.log('Sophie birthday page');
+
+    const user = {
+        email: 'email@gmail.com',
+        username: 'username',
+        firstName: 'firstname',
+        lastName: 'lastname',
+        birthday: 'birthday',
+        age: 20
+    }
+
+    return(
+        <div className='flex min-h-screen bg-background items-center justify-center'>
+            <SideBar/>
+
+            <div className="w-full px-24 sm:ml-64">
+                <div className='w-full'>
+                    <div className='flex align-center justify-between'>
+                        <p className='text-xl'>Upcoming Birthdays</p>
+
+                        <Button text={'ADD A BIRTHDAY'}
+                                color={'text-white'}
+                                size={'h-10 w-fit'}
+                                font={'text-xs text-center font-semibold'}
+                                bg={'bg-lightBlue'}
+                                padding={'px-8'}
+                                onClick={() => {}}
+                        />
+                    </div>
+                    <BirthdayCard user={user}/>
+                    <BirthdayCard user={user}/>
+                    <BirthdayCard user={user}/>
+                    <BirthdayCard user={user}/>
+                    <BirthdayCard user={user}/>
+                </div>
+            </div>
         </div>
-    );
+    )
 }
--- client/src/pages/gifts/Gifts.tsx
+++ client/src/pages/gifts/Gifts.tsx
@@ -1,7 +1,69 @@
+import * as React from "react";
+
+import Button from '../../components/UI/Button'
+import SideBar from "../../components/SideBar";
+import GiftCard from "../../components/GiftCard";
+
 export default function Gifts() {
-    return (
-        <div>
-            <h1>Coming Soon</h1>
+    console.log('Sophie gift page');
+
+    const user = {
+        email: 'email@gmail.com',
+        username: 'username',
+        firstName: 'firstname',
+        lastName: 'lastname',
+        birthday: 'birthday',
+        age: 20
+    }
+
+    const gift = {
+        name: 'Teddy Bear',
+        price: 20.99,
+        starred: false,
+        receiver: user
+    }
+
+    return(
+        <div className='min-h-screen bg-background flex justify-center items-center'>
+            <SideBar/>
+
+            <div className="px-40 py-20 sm:ml-64">
+                <div className='flex align-center justify-between'>
+                    <p className='text-xl'>My Gifts</p>
+
+                    <Button text={'ADD A GIFT'}
+                            color={'text-white'}
+                            size={'h-10 w-fit'}
+                            font={'text-xs text-center font-semibold'}
+                            bg={'bg-lightBlue'}
+                            padding={'px-8'}
+                            onClick={() => {
+                            }}
+                    />
+                </div>
+
+
+                <div className='flex flex-wrap gap-5 justify-between'>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                    <GiftCard gift={gift} deleteGift={() => {
+                    }}/>
+                </div>
+            </div>
         </div>
     );
 }
