8aed25021e6394ac49df98589cb5dd596944344d
sophiexie <sophiexie118@gmail.com>
2024-01-30 19:19:55 -0500

    Added styling and fixed page/component layouts

--- client/src/components/GiftCard.tsx
+++ client/src/components/GiftCard.tsx
@@ -14,16 +14,15 @@ export default function GiftCard({gift, deleteGift}: giftCardProps){
     const [openModal, setOpenModal] = useState(false);
 
     return(
-        <div className={"h-60 w-72 rounded-md mt-5 flex flex-col items-center justify-center px-5 py-3 bg-white shadow-md"}>
-            <div className='h-3/6 w-full flex flex-col bg-lightBlue justify-between items-stretch px-3 py-2 rounded-md'>
-                <p className='text-4xl font-bold text-white'>{gift.name}</p>
-                <p className='text-white text-end'>{gift.price}</p>
+        <div className={"h-64 max-w-80 min-w-72 rounded-md mt-5 flex flex-col items-center justify-center px-4 py-8 bg-white shadow-md"}>
+            <div className='bg-gradient-to-r from-blue200 to-blue100 h-3/6 w-full flex flex-col justify-between items-stretch px-3 py-2 rounded-md'>
+                <p className='font-dmSans text-4xl font-bold text-light'>{gift.name}</p>
+                <p className='font-dmSans text-light text-end'>{gift.price}</p>
             </div>
 
             <div className='w-full flex items-end'>
-
                 <div className='w-full mt-5'>
-                    <p className='text-xs mb-2'>Receiver</p>
+                    <p className='font-dmSans text-navy text-sm mb-2'>Receiver</p>
 
                     <div className='flex items-center'>
                         <img className='w-10 h-10 mr-4 rounded-3xl'
@@ -32,27 +31,28 @@ export default function GiftCard({gift, deleteGift}: giftCardProps){
                         />
 
                         <div className='mr-4'>
-                            <p className='text-black'>{gift.receiver ? formatFullName(gift.receiver) : 'Who\'s the lucky one?'}</p>
-                            <p className='text-darkGray'>{gift.receiver?.username}</p>
+                            <p className='font-dmSans font-medium text-navy'>{gift.receiver ? formatFullName(gift.receiver) : 'Who\'s the lucky one?'}</p>
+                            <p className='font-dmSans text-darkGray text-md'>{gift.receiver?.username}</p>
                         </div>
                     </div>
 
-                    <div className='flex gap-1 justify-end'>
+                    <div className='flex gap-1 justify-end mt-4'>
                         <Button text={'Edit'}
                                 color={'text-green'}
-                                size={'h-5 w-fit'}
-                                font={'text-xs text-center'}
+                                size={'h-6 w-fit'}
+                                font={'font-dmSans font-bold text-xs text-center'}
                                 bg={'bg-lightGreen'}
-                                padding={'px-2'}
+                                padding={'px-3'}
                                 onClick={() => console.log('Sophie edit gift')}
                         />
 
                         <Button text={'Delete'}
                                 color={'text-red'}
-                                size={'h-5 w-fit'}
-                                font={'text-xs text-center'}
+                                size={'h-6 w-fit'}
+                                font={'font-dmSans font-bold text-xs text-center'}
                                 bg={'bg-lightRed'}
-                                padding={'px-2'}
+                                padding={'px-3'}
+                                margin={'ml-1'}
                                 onClick={deleteGift}
                         />
                     </div>
@@ -61,4 +61,3 @@ export default function GiftCard({gift, deleteGift}: giftCardProps){
         </div>
     )
 }
-
--- client/src/pages/gifts/Gifts.tsx
+++ client/src/pages/gifts/Gifts.tsx
@@ -27,23 +27,22 @@ export default function Gifts() {
         <div className='min-h-screen bg-background flex justify-center items-center'>
             <SideBar/>
 
-            <div className="px-40 py-20 sm:ml-64">
-                <div className='flex align-center justify-between'>
-                    <p className='text-xl'>My Gifts</p>
-
+            <div className="px-10 py-20 sm:ml-64">
+                <div className='flex sm:flex-col md:flex-col lg:flex-row align-center justify-between mr-4'>
+                    <p className='font-dmSans text-2xl font-bold text-navy'>My Gifts</p>
                     <Button text={'ADD A GIFT'}
-                            color={'text-white'}
-                            size={'h-10 w-fit'}
-                            font={'text-xs text-center font-semibold'}
-                            bg={'bg-lightBlue'}
-                            padding={'px-8'}
+                            color={'text-light'}
+                            size={'h-10 w-36'}
+                            font={'font-dmSans font-bold text-md text-center'}
+                            bg={'bg-gradient-to-r from-blue200 to-blue100'}
+                            padding={'px-2'}
                             onClick={() => {
                             }}
                     />
                 </div>
 
 
-                <div className='flex flex-wrap gap-5 justify-between'>
+                <div className='flex flex-wrap gap-2'>
                     <GiftCard gift={gift} deleteGift={() => {
                     }}/>
                     <GiftCard gift={gift} deleteGift={() => {
@@ -60,8 +59,19 @@ export default function Gifts() {
                     }}/>
                     <GiftCard gift={gift} deleteGift={() => {
                     }}/>
-                    <GiftCard gift={gift} deleteGift={() => {
-                    }}/>
+                </div>
+
+                <div className='w-full flex justify-end items-end'>
+                    <Button text={'Next page'}
+                            color={'text-navy'}
+                            size={'h-fit w-fit'}
+                            font={'font-dmSans font-bold text-md text-end hover:underline'}
+                            bg={'bg-clear'}
+                            padding={'px-2'}
+                            margin={'my-4 mr-4'}
+                            onClick={() => {
+                            }}
+                    />
                 </div>
             </div>
         </div>
